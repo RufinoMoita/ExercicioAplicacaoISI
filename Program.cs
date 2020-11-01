faea137932573d6a178a968fdd2ca0c544d2e51c
Rui Costa <a17005@alunos.ipca.pt>
2020-11-01 18:18:49 +0000

    Terceira Tentativa Falhada

--- Program.cs
+++ Program.cs
@@ -5,23 +5,30 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Net.Http;
 using System.Collections.Generic;
-using System.Web;
-using System.Collections;
+using System.Xml;
+using System.Web.Script.Serialization;
+using System.Text.Json.Serialization;
 
 namespace ExercicioAplicacaoISI
 {
     class Program
     {
-        public static List<KeyValuePair<int, string>> lista = new List<KeyValuePair<int, string>>();
+        static Dictionary<int, string> listaLocais = new Dictionary<int, string>();
 
-        //private static async Task<string> GetData(string url)
-        //{
-        //    var httpClient = new HttpClient();
-        //    var resposta = await httpClient.GetAsync(url);
-        //    var result = await resposta.Content.ReadAsStringAsync();
+        static PrevisaoIPMA LerPrevisoes(int globalIdLocal)
+        {
+            // Instanciar JavaScriptSerializer para converter o objeto para JSON
+            JavaScriptSerializer javaScriptSerializer = new JavaScriptSerializer();
 
-        //    return result;
-        //}
+            string json = null;
+            //using (StreamReader reader = new StreamReader("data_forecast/" + globalIdLocal + ".json")) ;
+            string jsonPath = Path.GetFullPath(Path.Combine("data_forecast/" + globalIdLocal + ".json"));
+            json = File.ReadAllText(jsonPath);
+
+            PrevisaoIPMA obj = javaScriptSerializer.Deserialize<PrevisaoIPMA>(json);
+            obj.local = listaLocais[obj.globalIdLocal];
+            return obj;
+        }
 
         static Dictionary<int, string> LerLocais(string ficheiro)
         {
@@ -29,85 +36,67 @@ namespace ExercicioAplicacaoISI
             Dictionary<int, string> dicLocais = new Dictionary<int, string>();
 
             // Expressão Regular para instanciar objeto Regex
-            String erString = @"^[0-9]{7},[123],([1-9]?\d,){2}[A-Z]{3},([^,\n]*)$";
-
-
-            // -> Processar o conteúdo do ficheiro
-            // -----------------------------------------------
-            // Alternativa 01: ler tudo da Stream para uma string 
-            //                 e depois passa a processar o texto da string
-            string csvString = null;
-            using (StreamReader reader = new StreamReader(ficheiro))
-            {
-                csvString = reader.ReadToEnd();
-            }
-            MatchCollection matches = Regex.Matches(csvString, erString,
-                                                     RegexOptions.Multiline);
-            // RegexOptions.Multiline permite interpretar a String com tendo várias 
-            // linhas (caracter '\n'), permitindo usar os operadores de início e fim de linha  ^ $
-
-            foreach (Match m in matches)
-            {
-                // m corresponderá ao conteúdo de cada linha do ficheiro (considerando que respeita a ER)  
-                Console.WriteLine(m.Value);
-            }
-            // FIM da alternativa 01
+            String erString = @"^[0-9]+,[0-9],[0-9]+,[0-9]+,[A-Z]{3},[A-Z][a-z]+$";
 
 
             // Alternativa 02: depois de ler o conteúdo do ficheiro para uma stream, 
             //                    vai acedendo "linha-a-linha"
-            //    Regex g = new Regex(erString);
-            //    using (StreamReader r = new StreamReader(ficheiro))
-            //    {
-            //        string line;
+            Regex g = new Regex(erString);
+            using (StreamReader r = new StreamReader(ficheiro))
+            {
+                string line;
 
-            //        while ((line = r.ReadLine()) != null)
-            //        {
-            //            // Tenta correspondência (macthing) da ER com cada linha do ficheiro
-            //            Match m = g.Match(line);
-            //            if (m.Success)
-            //            {
-            //                //  estrutura de cada linha com correspondência:
-            //                //      globalIdLocal,idRegiao,idDistrito,idConcelho,idAreaAviso,local
-            //                //  separar pelas vírgulas...
-            //                string[] campos = m.Value.Split(',');
-            //                int codLocal = int.Parse(campos[0]);
-            //                string cidade = campos[5];
-            //                // Guardar na estrutura de dados dicionário
-            //                // dicLocais.Add( CHAVE ,  VALOR )
-            //                dicLocais.Add(codLocal, cidade);
-            //            }
-            //            else
-            //            {
-            //                Console.WriteLine($"Linha inválida: {line}");
-            //            }
-            //        }
-            //    }
+                while ((line = r.ReadLine()) != null)
+                {
+                    // Tenta correspondência (macthing) da ER com cada linha do ficheiro
+                    Match m = g.Match(line);
+                    if (m.Success)
+                    {
+                        //  estrutura de cada linha com correspondência:
+                        //      globalIdLocal,idRegiao,idDistrito,idConcelho,idAreaAviso,local
+                        //  separar pelas vírgulas...
+                        string[] campos = m.Value.Split(',');
+                        int codLocal = int.Parse(campos[0]);
+                        string cidade = campos[5];
+                        // Guardar na estrutura de dados dicionário
+                        // dicLocais.Add( CHAVE ,  VALOR )
+                        dicLocais.Add(codLocal, cidade);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Linha inválida: {line}");
+                    }
+                }
+            }
             return dicLocais;
-           }
+        }
 
         static void Main(string[] args)
         {
-            int i = 0;
-            Dictionary<int, string> dicLocais = LerLocais("../../locais.csv");
+            Dictionary<int, string> dicLocais = LerLocais(@"../../locais.csv");
+
+            // Instanciar JavaScriptSerializer para converter o objeto para JSON
+            JavaScriptSerializer javaScriptSerializer = new JavaScriptSerializer();
+
             foreach (KeyValuePair<int, string> kv in dicLocais)
             {
-
-                lista[i] = kv;
-                //lista.Add(kv);
-                //lista.Insert(i, kv);
-
                 Console.WriteLine($"globalIdLocal = {kv.Key} cidade = {kv.Value}");
-                Console.WriteLine($"globalIdLocal = {lista[i].Key} cidade = {lista[i].Value}");
-                i++;
+
+                //Ler previsao para cada regiao
+                PrevisaoIPMA previsao = LerPrevisoes(kv.Key);
+
+                //Atribuir o nome do local à previsão
+                previsao.local = kv.Value;
+
+                var json = javaScriptSerializer.Serialize(previsao);
+
+                if (!File.Exists(kv.Key + "-detalhes.json"))
+                {
+                    File.WriteAllText(kv.Key + "-detalhes.json", json);
+                }
+
             }
-
-            Console.WriteLine("\n\n");
             Console.ReadKey();
-
-            ExercicioAplicacaoISI.JSON.GravaJson();
-
-
         }
 
         }
